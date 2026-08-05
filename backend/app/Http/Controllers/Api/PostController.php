b6384506e4a58f7803ae906be9580b9e4511fe47
Bitten07 <joaopedrobittensilva@gmail.com>
2026-08-05 16:58:15 -0300

    postController

---
+++ backend/app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Service;
+
+use App\Models\Post;
+use Illuminate\Support\Facades\Storage;
+
+class PostController extends Controller
+{
+    
+}
---
+++ backend/app/Services/PostService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Post;
+use Illuminate\Support\Facades\Storage;
+
+class PostService
+{
+    
+}
